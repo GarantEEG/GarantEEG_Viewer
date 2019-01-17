fb169fa6e031439244675c4878e9f2e0ffea41c5
Hotride <la2sw@list.ru>
2019-01-17 13:17:08 +0300

    Добавил функционал для использования GarantEEG API.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,12 +1,32 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QDebug>
+
 MainWindow::MainWindow(QWidget *parent)
 : QMainWindow(parent), ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
 
     m_Eeg = GarantEEG::CreateDevice(GarantEEG::DT_GARANT);
+
+    if (m_Eeg != nullptr)
+    {
+        m_Eeg->SetCallback_OnStartStateChanged([](uint state)
+        {
+            qDebug() << "StartStateChanged" << state;
+        });
+
+        m_Eeg->SetCallback_OnRecordingStateChanged([](uint state)
+        {
+            qDebug() << "RecordingStateChanged" << state;
+        });
+
+        m_Eeg->SetCallback_ReceivedData([](const GarantEEG::GARANT_EEG_DATA *eegData)
+        {
+            qDebug() << "ReceivedData";
+        });
+    }
 }
 
 MainWindow::~MainWindow()
@@ -19,10 +39,19 @@ void MainWindow::on_pb_Start_clicked()
      if (m_Eeg == nullptr)
          return;
 
-     if (m_Eeg->Start())
+     if (m_Eeg->Start(true, 500, true, qPrintable(ui->le_IP->text()), ui->sb_Port->value()))
      {
          ui->pb_Start->setEnabled(false);
          ui->pb_Stop->setEnabled(true);
+
+         ui->pb_PauseStream->setEnabled(true);
+         ui->pb_ResumeStream->setEnabled(false);
+
+         ui->pb_RecordStart->setEnabled(true);
+         ui->pb_RecordStop->setEnabled(false);
+
+         ui->pb_RecordPause->setEnabled(false);
+         ui->pb_RecordResume->setEnabled(false);
      }
 }
 
@@ -35,4 +64,93 @@ void MainWindow::on_pb_Stop_clicked()
 
     ui->pb_Start->setEnabled(true);
     ui->pb_Stop->setEnabled(false);
+
+    ui->pb_PauseStream->setEnabled(false);
+    ui->pb_ResumeStream->setEnabled(false);
+
+    ui->pb_RecordStart->setEnabled(false);
+    ui->pb_RecordStop->setEnabled(false);
+
+    ui->pb_RecordPause->setEnabled(false);
+    ui->pb_RecordResume->setEnabled(false);
+}
+
+void MainWindow::on_pb_PauseStream_clicked()
+{
+    if (m_Eeg == nullptr || !m_Eeg->IsStarted() || m_Eeg->IsRecording() || m_Eeg->IsPaused())
+        return;
+
+    m_Eeg->StopDataTranslation();
+    ui->pb_PauseStream->setEnabled(false);
+    ui->pb_ResumeStream->setEnabled(true);
+
+    ui->pb_RecordStart->setEnabled(false);
+    ui->pb_RecordStop->setEnabled(false);
+
+    ui->pb_RecordPause->setEnabled(false);
+    ui->pb_RecordResume->setEnabled(false);
+}
+
+void MainWindow::on_pb_ResumeStream_clicked()
+{
+    if (m_Eeg == nullptr || !m_Eeg->IsStarted() || m_Eeg->IsRecording() || !m_Eeg->IsPaused())
+        return;
+
+    m_Eeg->StartDataTranslation();
+    ui->pb_PauseStream->setEnabled(true);
+    ui->pb_ResumeStream->setEnabled(false);
+
+    ui->pb_RecordStart->setEnabled(true);
+    ui->pb_RecordStop->setEnabled(false);
+
+    ui->pb_RecordPause->setEnabled(false);
+    ui->pb_RecordResume->setEnabled(false);
+}
+
+void MainWindow::on_pb_RecordStart_clicked()
+{
+    if (m_Eeg == nullptr || !m_Eeg->IsStarted() || m_Eeg->IsRecording() || m_Eeg->IsPaused())
+        return;
+
+    if (m_Eeg->StartRecord("user"))
+    {
+        ui->pb_RecordStart->setEnabled(false);
+        ui->pb_RecordStop->setEnabled(true);
+
+        ui->pb_RecordPause->setEnabled(true);
+        ui->pb_RecordResume->setEnabled(false);
+    }
+}
+
+void MainWindow::on_pb_RecordStop_clicked()
+{
+    if (m_Eeg == nullptr || !m_Eeg->IsStarted() || !m_Eeg->IsRecording() || m_Eeg->IsPaused())
+        return;
+
+    m_Eeg->StopRecord();
+    ui->pb_RecordStart->setEnabled(true);
+    ui->pb_RecordStop->setEnabled(false);
+
+    ui->pb_RecordPause->setEnabled(false);
+    ui->pb_RecordResume->setEnabled(false);
+}
+
+void MainWindow::on_pb_RecordPause_clicked()
+{
+    if (m_Eeg == nullptr || !m_Eeg->IsStarted() || !m_Eeg->IsRecording() || m_Eeg->IsRecordPaused())
+        return;
+
+    m_Eeg->PauseRecord();
+    ui->pb_RecordPause->setEnabled(false);
+    ui->pb_RecordResume->setEnabled(true);
+}
+
+void MainWindow::on_pb_RecordResume_clicked()
+{
+    if (m_Eeg == nullptr || !m_Eeg->IsStarted() || !m_Eeg->IsRecording() || !m_Eeg->IsRecordPaused())
+        return;
+
+    m_Eeg->ResumeRecord();
+    ui->pb_RecordPause->setEnabled(true);
+    ui->pb_RecordResume->setEnabled(false);
 }
--- mainwindow.h
+++ mainwindow.h
@@ -21,6 +21,18 @@ private slots:
 
     void on_pb_Stop_clicked();
 
+    void on_pb_PauseStream_clicked();
+
+    void on_pb_ResumeStream_clicked();
+
+    void on_pb_RecordStart_clicked();
+
+    void on_pb_RecordStop_clicked();
+
+    void on_pb_RecordPause_clicked();
+
+    void on_pb_RecordResume_clicked();
+
 private:
     Ui::MainWindow *ui;
 
